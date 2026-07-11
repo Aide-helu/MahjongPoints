57af79e306e124bd41a690ddc5017588058a9d71
Aide-helu <114740256+Aide-helu@users.noreply.github.com>
2026-07-12 01:57:34 +0800

    增加预览便与热重载

--- MahjongPoints.Android/MainActivity.cs
+++ MahjongPoints.Android/MainActivity.cs
@@ -5,6 +5,7 @@ namespace MahjongPoints.Android;
 [global::Android.App.Activity(
     Label = "MahjongPoints",
     MainLauncher = true,
+    Theme = "@style/AppTheme",
     ConfigurationChanges =
         global::Android.Content.PM.ConfigChanges.Orientation |
         global::Android.Content.PM.ConfigChanges.ScreenSize |
---
+++ MahjongPoints.Preview/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MahjongPoints.Preview;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
